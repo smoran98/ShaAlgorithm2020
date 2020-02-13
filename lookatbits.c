4c611f294d2a1c9310469b66bdf427555bf3b010
Shane Moran <G00338607@gmit.ie>
2020-02-13 20:09:59 +0000

    C Bit Operations

---
+++ lookatbits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+
+int main (int argc, char *argv[]){
+
+  char c = 65;
+  int i = 65;
+  long int l = 65;
+  long long int m = 65;
+  uint8_t e = 65;
+  uint64_t s = 65;
+printf("%x\t%c\t%d\t%d\n", c, c, sizeof(c));
+printf("%x\t%c\t%d\t%d\n", i ,i, sizeof(i));
+printf("%x\t%c\t%d\t%d\n", l, l, sizeof(l));
+printf("%x\t%c\t%d\t%d\n", m, m, sizeof(m));
+return 0;
+
+}
+
