e2e2842aed7ff850db405af17d88d724e477546a
Shane Moran <G00338607@gmit.ie>
2020-03-03 18:02:37 +0000

    Merging padding with hashing

--- Sha256.c
+++ Sha256.c
@@ -112,6 +112,23 @@ void nexthash(union block *M, uint32_t *H) {
 
 }
 
+int nextblock(union block *M, FILE *infile) {
+  for (*nobits = 0, i = 0; fread(&M.eight[i], 1, 1, infile) == 1; *nobits += 8) {
+  printf("Maj(x,y,z) = %08x\n", Maj(x, y, z));	    
+  printf("%02" PRIx8, M.eight[i]);
+  }
+
+  printf("%02" PRIx8, 0x80); // Bits: 1000 0000
+
+
+  printf("SHR(x,4)   = %08x\n", SHR(x, 4));	 
+  
+  for (uint64_t i = nozerobytes(*nobits); i > 0; i--)
+  printf("ROTR(x,4)  = %08x\n", ROTR(x, 4));	    
+  printf("%02" PRIx8, 0x00);
+  
+}
+
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
@@ -137,9 +154,9 @@ int main(int argc, char *argv[]) {
   enum flag status = READ;
 
   // Read through all of the padded message blocks.
-  while (nextblock(&M, infile, nobits, status)) {
+  while (M = nextblock()) {
     // Calculate the next hash value.
-    nexthash(&M, &H);
+    H = nexthash(M, H);
   }
 
   for (int i = 0; i < 8; i++)
@@ -150,3 +167,64 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+// Shane Moran
+// Padding Sha256
+
+#include <stdio.h>
+#include <inttypes.h>
+
+union block {
+    uint64_t sixFour[8]; // 64 * 8 = 512
+    uint32_t threeTwo[16]; // 32 * 16 = 512
+    uint8_t eight[64]; // 8 * 64 = 512
+}; 
+
+
+uint64_t no_zeros_bytes(uint64_t no_bits) {
+    uint64_t result = 512 - (no_bits % 512ULL);
+
+    if (result < 65) {
+        result += 512;
+    }
+
+    result -= 72;
+
+    return (result / 8ULL);
+}
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 2) {
+        printf("Error: expected single filename as argument.\n");
+        return 1;
+    }
+
+    FILE *inFile = fopen(argv[1], "rb");
+
+    // Error handling
+    if (!inFile) {
+        printf("Error: couldn't open file %s. \n", argv[1]);
+        return 1;
+    }
+
+    uint64_t noBits;
+    union block M;
+    uint8_t i;
+
+    for (noBits = 0, i = 0; fread(&M.eight[i], 1, 1, inFile) == 1; noBits += 8) {
+        printf("%02" PRIx8, M.eight[i]);
+    }
+
+    printf("%02" PRIx8, 0x80); // Bits: 1000 0000
+
+    for (uint64_t i = (no_zeros_bytes(noBits)); i > 0; i--) {
+        printf("%02" PRIx8, 0x00);
+    }
+
+    printf("%016" PRIx64 "\n", noBits);
+    printf("\n");
+    fclose(inFile);
+
+ return 0;
+
+}
