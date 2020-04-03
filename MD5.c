5452a87e16b1ff74528a276860f689596f89c209
Shane Moran <shane906.sm@gmail.com>
2020-04-03 14:20:40 +0100

    heading

--- MD5.c
+++ MD5.c
@@ -31,11 +31,34 @@ uint32_t I(uint32_t x, uint32_t y, uint32_t z)
 }
 
 
+void heading()
+{
+    printf("\n G00338607 - Shane Moran");
+    printf("\n MD5 Hash Digest Algorithm");
+}
+
 
 int main() {
-    // Output string
+    // Output string & heading
+    heading();
+    
+    printf("\n");
     printf("Hello, World!");
     printf("\n");
     printf("K[2]         = %08x\n", K[2]);
+
+    uint32_t x = K[0];
+    uint32_t y = K[1];
+    uint32_t z = K[2];
+
+    printf("x  K[0] = %08x\n", K[0]);
+    printf("y  K[1] = %08x\n", K[1]);
+    printf("z  K[2] = %08x\n", K[2]);
+
+    printf("F(x,y,z) = %08x\n", F(K[0], K[1], K[2]));
+    printf("G(x,y,z) = %08x\n", G(K[0], K[1], K[2]));
+    printf("H(x,y,z) = %08x\n", H(K[0], K[1], K[2]));
+    printf("I(x,y,z) = %08x\n", I(K[0], K[1], K[2]));
+
     return 0;
 }
