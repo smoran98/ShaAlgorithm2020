ac543bac9759ae8a107564540087ac54c86b3366
Shane Moran <shane906.sm@gmail.com>
2020-04-03 13:15:41 +0100

    Basic CLion Program

---
+++ MD5.c
@@ -0,0 +1,10 @@
+// Theory of Algorithms Projects
+// Created by Shane Moran on 03/04/2020.
+// All Online Resources & References can be found in the README.md
+
+#include <stdio.h>
+int main() {
+    // Output string
+    printf("Hello, World!");
+    return 0;
+}
