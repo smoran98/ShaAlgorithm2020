b60dc4d7f35f33dd7a301b949d412ba477decd17
Shane Moran <shane906.sm@gmail.com>
2020-04-03 13:14:50 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ShaAlgorithm2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShaAlgorithm2020.dir/MD5.c.obj"
+  "ShaAlgorithm2020.exe"
+  "ShaAlgorithm2020.exe.manifest"
+  "ShaAlgorithm2020.lib"
+  "ShaAlgorithm2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ShaAlgorithm2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
