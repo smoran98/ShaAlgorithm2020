c1f7cbaad180bb4096628466a9c94602e043553c
Shane Moran <shane906.sm@gmail.com>
2020-04-03 13:56:18 +0100

    Constants

--- MD5.c
+++ MD5.c
@@ -1,10 +1,41 @@
 // Theory of Algorithms Projects
 // Created by Shane Moran on 03/04/2020.
-// All Online Resources & References can be found in the README.md
+// All Online Resources & References can be found in the README.MD
 
 #include <stdio.h>
+#include <stdint.h>
+
+// constants
+const uint32_t K[] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5};
+uint32_t y[23];
+
+// func's
+uint32_t F(uint32_t x, uint32_t y, uint32_t z)
+{
+    return (x & y) | (~x & z);
+}
+
+uint32_t G(uint32_t x, uint32_t y, uint32_t z)
+{
+    return (x & y) | (y & ~z);
+}
+
+uint32_t H(uint32_t x, uint32_t y, uint32_t z)
+{
+    return (x ^ y ^ z);
+}
+
+uint32_t I(uint32_t x, uint32_t y, uint32_t z)
+{
+    return (y ^ (x | ~z));
+}
+
+
+
 int main() {
     // Output string
     printf("Hello, World!");
+    printf("\n");
+    printf("K[2]         = %08x\n", K[2]);
     return 0;
 }
