f31a625e70a6fdd98f613ce9e16a1d32b9ac7731
Shane Moran <shane906.sm@gmail.com>
2020-04-04 18:36:34 +0100

    Functions defined n nexthash errors

--- MD5.c
+++ MD5.c
@@ -2,20 +2,23 @@
 // Created by Shane Moran on 03/04/2020.
 // All Online Resources & References can be found in the README.MD
 
+#pragma region IMPORTS
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#pragma endregion
 
-
+#pragma region PREPROCESSING SETUP
 // 64-byte block of memory, accessed via different types
 typedef union {
     uint64_t sixfour[8];
     uint32_t threetwo[16];
     uint8_t eight[64];
 } BLOCK;
+#pragma endregion
 
-
+#pragma region PARSE STATUS
 // Keep track of where we are in padding the message.
 typedef enum {READ, PAD0, FINISH} PADFLAG;
 
@@ -23,26 +26,34 @@ typedef enum {READ, PAD0, FINISH} PADFLAG;
 const uint32_t K[] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5};
 
 // func's
-uint32_t F(uint32_t x, uint32_t y, uint32_t z)
-{
-    return (x & y) | (~x & z);
-}
+//uint32_t F(uint32_t x, uint32_t y, uint32_t z)
+//{
+//    return (x & y) | (~x & z);
+//}
+#define F(x,y,z) (((x) & (y)) | ((~x) & (z)))
 
-uint32_t G(uint32_t x, uint32_t y, uint32_t z)
-{
-    return (x & y) | (y & ~z);
-}
+//uint32_t G(uint32_t x, uint32_t y, uint32_t z)
+//{
+//    return (x & y) | (y & ~z);
+//}
+#define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
 
-uint32_t H(uint32_t x, uint32_t y, uint32_t z)
-{
-    return (x ^ y ^ z);
-}
+//uint32_t H(uint32_t x, uint32_t y, uint32_t z)
+//{
+//    return (x ^ y ^ z);
+//}
+#define H(x, y, z) ((x) ^ (y) ^ (z))
 
-uint32_t I(uint32_t x, uint32_t y, uint32_t z)
-{
-    return (y ^ (x | ~z));
-}
+//uint32_t I(uint32_t x, uint32_t y, uint32_t z)
+//{
+//    return (y ^ (x | ~z));
+//}
+#define I(x, y, z) ((y) ^ ((x) | (~z)))
 
+
+#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+
+#pragma endregion
 
 uint64_t no_zero_bytes(uint64_t nobytes)
 {
@@ -100,6 +111,30 @@ int nextblock(BLOCK *M, FILE *infile, uint64_t *nobits, enum PADFLAG *status) {
 
     return 1;
 
+}
+
+// Section 6.2.2
+void nexthash(WORD *M, WORD *H) {
+
+    WORD W[64];
+    WORD a, b, c, d, e, f, g, h, T1, T2;
+    int t;
+
+    for (t = 0; t < 16; t++)
+        W[t] = M[t];
+
+    for (t = 16; t < 64; t++)
+        W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+
+    a = H[0]; b = H[1]; c = H[2]; d = H[3];
+    e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+    for (t = 0; t < 64; t++) {
+        T1 = h + Sig1(e) + Ch(e, f, g) + K[t] + W[t];
+        T2 = Sig0(a) + Maj(a, b, c);
+        h = g; g = f; f = e; e = d + T1;
+        d = c; c = b; b = a; a = T1 + T2;
+    }
 }
 
 
