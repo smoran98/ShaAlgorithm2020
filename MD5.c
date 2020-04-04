14c7b8cf809f994f2ad601b6813f1ae2cfa8e64e
Shane Moran <shane906.sm@gmail.com>
2020-04-04 17:38:43 +0100

    SHA256 Video/File

--- MD5.c
+++ MD5.c
@@ -4,10 +4,23 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+
+// 64-byte block of memory, accessed via different types
+typedef union {
+    uint64_t sixfour[8];
+    uint32_t threetwo[16];
+    uint8_t eight[64];
+} BLOCK;
+
+
+// Keep track of where we are in padding the message.
+typedef enum {READ, PAD0, FINISH} PADFLAG;
 
 // constants
 const uint32_t K[] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5};
-uint32_t y[23];
 
 // func's
 uint32_t F(uint32_t x, uint32_t y, uint32_t z)
@@ -31,6 +44,65 @@ uint32_t I(uint32_t x, uint32_t y, uint32_t z)
 }
 
 
+uint64_t no_zero_bytes(uint64_t nobytes)
+{
+    uint64_t no = 512 - (nobytes % 512ULL);
+
+    if (no < 65)
+        no += 512;
+
+    no -= 72;
+
+    return (no / 8ULL);
+}
+
+
+// Section 5.1.1 - message input from infile.
+int nextblock(BLOCK *M, FILE *infile, uint64_t *nobits, enum PADFLAG *status) {
+
+    int i;
+    size_t nobytesread;
+
+    switch(*status){
+        case FINISH:
+            return 0;
+        case PAD0:
+            // We need an all-padding block without the 1 bit.
+            for (int i = 0; i < 56; i++)
+                M->eight[i] = 0x00;
+            M->sixfour[7] = htobe64(*nobits);
+            *status = FINISH;
+            break;
+        default:
+            // Try to read 64 bytes from the file.
+            nobytesread = fread(M->eight, 1, 64, infile);
+            *nobits += (8ULL * ((uint64_t) nobytesread));
+
+            if (nobytesread < 56) {
+                // We can put all padding in this block.
+                M->eight[nobytesread] = 0x80;
+                for (i = nobytesread + 1; i < 56; i++)
+                    M->eight[i] = 0x00;
+                M->sixfour[7] = htobe64(*nobits);
+                *status = FINISH;
+            } else if (nobytesread < 64) {
+                // Otherwise we have read between 56 (incl) and 64 (excl) bytes.
+                M->eight[nobytesread] = 0x80;
+                for (int i = nobytesread + 1; i < 64; i++)
+                    M->eight[i] = 0x00;
+                *status = PAD0;
+            }
+    }
+
+    // Convert to host endianess, word-size-wise.
+    for (i = 0; i < 16; i++)
+        M->threetwo[i] = be32toh(M->threetwo[i]);
+
+    return 1;
+
+}
+
+
 void heading()
 {
     printf("\n G00338607 - Shane Moran");
@@ -38,10 +110,10 @@ void heading()
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
     // Output string & heading
     heading();
-    
+
     printf("\n");
     printf("Hello, World!");
     printf("\n");
