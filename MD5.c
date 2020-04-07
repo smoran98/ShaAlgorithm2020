431c2c4ed38c80c987a17c6cf063909497bbc484
Shane Moran <shane906.sm@gmail.com>
2020-04-07 11:48:51 +0000

    Rounds

--- MD5.c
+++ MD5.c
@@ -48,6 +48,28 @@ enum PADFLAG {READ, PAD0, FINISH} ;
 
 #define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
 
+// rounds 1-4
+#define FF(a, b, c, d, x, s, ac) {\
+ (a) += F ((b), (c), (d)) + (x) + (UINT4)(ac);\
+ (a) = ROTATE_LEFT ((a), (s));\
+ (a) += (b);\
+  }
+#define GG(a, b, c, d, x, s, ac) {\
+ (a) += G ((b), (c), (d)) + (x) + (UINT4)(ac);\
+ (a) = ROTATE_LEFT ((a), (s));\
+ (a) += (b);\
+  }
+#define HH(a, b, c, d, x, s, ac) {\
+ (a) += H ((b), (c), (d)) + (x) + (UINT4)(ac);\
+ (a) = ROTATE_LEFT ((a), (s));\
+ (a) += (b);\
+  }
+#define II(a, b, c, d, x, s, ac) {\
+ (a) += I ((b), (c), (d)) + (x) + (UINT4)(ac);\
+ (a) = ROTATE_LEFT ((a), (s));\
+ (a) += (b);\
+  }
+
 
 // sin func table
 const uint32_t K[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
@@ -129,32 +151,89 @@ int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum PADFLAG *stat
 int nexthash(union block *M, uint32_t *H)
 {
     // var's
-    uint32_t W[64];
-    uint32_t A, AA, B, BB, C, CC, D, DD;
-    A = K[0];
-    B = K[1];
-    C = K[2];
-    D = K[3];
-    int t;
-    int s;
+    uint32_t a, b, c, d;
 
+    a = H[0];
+    b = H[1];
+    c = H[2];
+    d = H[3];
 
-    for (t = 0; t < 16; t++)
-    {
+    // R1
+    FF(a, b, c, d, M->threetwo[0], S11,  0xd76aa478);
+    FF(d, a, b, c, M->threetwo[1], S12,  0xe8c7b756);
+    FF(c, d, a, b, M->threetwo[2], S13,  0x242070db);
+    FF(b, c, d, a, M->threetwo[3], S14,  0xc1bdceee);
+    FF(a, b, c, d, M->threetwo[4], S11,  0xf57c0faf);
+    FF(d, a, b, c, M->threetwo[5], S12,  0x4787c62a);
+    FF(c, d, a, b, M->threetwo[6], S13,  0xa8304613);
+    FF(b, c, d, a, M->threetwo[7], S14,  0xfd469501);
+    FF(a, b, c, d, M->threetwo[8], S11,  0x698098d8);
+    FF(d, a, b, c, M->threetwo[9], S12,  0x8b44f7af);
+    FF(c, d, a, b, M->threetwo[10], S13, 0xffff5bb1);
+    FF(b, c, d, a, M->threetwo[11], S14, 0x895cd7be);
+    FF(a, b, c, d, M->threetwo[12], S11, 0x6b901122);
+    FF(d, a, b, c, M->threetwo[13], S12, 0xfd987193);
+    FF(c, d, a, b, M->threetwo[14], S13, 0xa679438e);
+    FF(b, c, d, a, M->threetwo[15], S14, 0x49b40821);
 
-        for (s = 16; s < 64; s++)
-        {
-            W[s] = M->threetwo[t * 16 + s];
-        }
+    // R2
+    GG(a, b, c, d, M->threetwo[1], S21,  0xf61e2562);
+    GG(d, a, b, c, M->threetwo[6], S22,  0xc040b340);
+    GG(c, d, a, b, M->threetwo[11], S23, 0x265e5a51);
+    GG(b, c, d, a, M->threetwo[0], S24,  0xe9b6c7aa);
+    GG(a, b, c, d, M->threetwo[5], S21,  0xd62f105d);
+    GG(d, a, b, c, M->threetwo[10], S22, 0x2441453);
+    GG(c, d, a, b, M->threetwo[15], S23, 0xd8a1e681);
+    GG(b, c, d, a, M->threetwo[4], S24,  0xe7d3fbc8);
+    GG(a, b, c, d, M->threetwo[9], S21,  0x21e1cde6);
+    GG(d, a, b, c, M->threetwo[14], S22, 0xc33707d6);
+    GG(c, d, a, b, M->threetwo[3], S23,  0xf4d50d87);
+    GG(b, c, d, a, M->threetwo[8], S24,  0x455a14ed);
+    GG(a, b, c, d, M->threetwo[13], S21, 0xa9e3e905);
+    GG(d, a, b, c, M->threetwo[2], S22,  0xfcefa3f8);
+    GG(c, d, a, b, M->threetwo[7], S23,  0x676f02d9);
+    GG(b, c, d, a, M->threetwo[12], S24, 0x8d2a4c8a);
 
-        AA = A;
-        BB = B;
-        CC = C;
-        DD = D;
+    // R3
+    HH(a, b, c, d, M->threetwo[5], S31,  0xfffa3942);
+    HH(d, a, b, c, M->threetwo[8], S32,  0x8771f681);
+    HH(c, d, a, b, M->threetwo[11], S33, 0x6d9d6122);
+    HH(b, c, d, a, M->threetwo[14], S34, 0xfde5380c);
+    HH(a, b, c, d, M->threetwo[1], S31,  0xa4beea44);
+    HH(d, a, b, c, M->threetwo[4], S32,  0x4bdecfa9);
+    HH(c, d, a, b, M->threetwo[7], S33,  0xf6bb4b60);
+    HH(b, c, d, a, M->threetwo[10], S34, 0xbebfbc70);
+    HH(a, b, c, d, M->threetwo[13], S31, 0x289b7ec6);
+    HH(d, a, b, c, M->threetwo[0], S32,  0xeaa127fa);
+    HH(c, d, a, b, M->threetwo[3], S33,  0xd4ef3085);
+    HH(b, c, d, a, M->threetwo[6], S34,  0x4881d05);
+    HH(a, b, c, d, M->threetwo[9], S31,  0xd9d4d039);
+    HH(d, a, b, c, M->threetwo[12], S32, 0xe6db99e5);
+    HH(c, d, a, b, M->threetwo[15], S33, 0x1fa27cf8);
+    HH(b, c, d, a, M->threetwo[2], S34,  0xc4ac5665);
 
-        return 1;
-    }
-    return 0;
+    // R4
+    II(a, b, c, d, M->threetwo[0], S41, 0xf4292244);
+    II(d, a, b, c, M->threetwo[7], S42, 0x432aff97);
+    II(c, d, a, b, M->threetwo[14], S43, 0xab9423a7);
+    II(b, c, d, a, M->threetwo[5], S44, 0xfc93a039);
+    II(a, b, c, d, M->threetwo[12], S41, 0x655b59c3);
+    II(d, a, b, c, M->threetwo[3], S42, 0x8f0ccc92);
+    II(c, d, a, b, M->threetwo[10], S43, 0xffeff47d);
+    II(b, c, d, a, M->threetwo[1], S44, 0x85845dd1);
+    II(a, b, c, d, M->threetwo[8], S41, 0x6fa87e4f);
+    II(d, a, b, c, M->threetwo[15], S42, 0xfe2ce6e0);
+    II(c, d, a, b, M->threetwo[6], S43, 0xa3014314);
+    II(b, c, d, a, M->threetwo[13], S44, 0x4e0811a1);
+    II(a, b, c, d, M->threetwo[4], S41, 0xf7537e82);
+    II(d, a, b, c, M->threetwo[11], S42, 0xbd3af235);
+    II(c, d, a, b, M->threetwo[2], S43, 0x2ad7d2bb);
+    II(b, c, d, a, M->threetwo[9], S44, 0xeb86d391);
+
+    H[0] += a;
+    H[1] += b;
+    H[2] += c;
+    H[3] += d;
 }
 
 
