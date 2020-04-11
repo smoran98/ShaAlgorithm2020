2875b0c191b1b2bcb64a701314eb2c24de19c160
Shane Moran <shane906.sm@gmail.com>
2020-04-11 17:13:54 +0000

    Outputs Hash value

--- MD5.c
+++ MD5.c
@@ -17,7 +17,7 @@ union block{
 };
 
 // Keep track of where we are in padding the message.
-enum PADFLAG {READ, PAD0, FINISH} ;
+enum flag {READ, PAD0, FINISH} ;
 
 /* Constants for MD5Transform routine.*/
 #define S11 7
@@ -101,11 +101,18 @@ static unsigned char PADDING[64] = {
 
 
 // Section 5.1.1 - message input from infile.
-int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum PADFLAG *status) {
+int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status) {
 
     // var's
     int i;
     size_t nobytesread;
+
+    // Try to read 64 bytes from the file.
+    nobytesread = fread(M->eight, 1, 64, infile);
+    *nobits += nobytesread * 8;
+
+    if (*status == FINISH)
+        return 0;
 
     // We need an all-padding block without the 1 bit.
     if (*status == PAD0) {
@@ -117,10 +124,10 @@ int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum PADFLAG *stat
         return 1;
     }
 
-    // Try to read 64 bytes from the file.
-    nobytesread = fread(M->eight, 1, 64, infile);
-    //*nobits += (8ULL * ((uint64_t) nobytesread));
-    if (nobytesread == 56) {
+    if (nobytesread == 64)
+        return 1;
+
+    if (nobytesread < 56) {
         // We can put all padding in this block.
         M->eight[nobytesread] = 0x80;
         for (i = nobytesread + 1; i < 56; i++)
@@ -141,92 +148,95 @@ int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum PADFLAG *stat
 }
 
 // Section 6.2.2
-int nexthash(union block *M, uint32_t *H)
+static void nexthash(union block *M, uint32_t *Hash)
 {
     // var's
-    uint32_t a, b, c, d;
+    uint32_t H[4];
 
-    a = H[0];
-    b = H[1];
-    c = H[2];
-    d = H[3];
+    Hash[0] = H[0];
+    Hash[1] = H[1];
+    Hash[2] = H[2];
+    Hash[3] = H[3];
 
     // R1
-    FF(a, b, c, d, M->threetwo[0], S11,  0xd76aa478);
-    FF(d, a, b, c, M->threetwo[1], S12,  0xe8c7b756);
-    FF(c, d, a, b, M->threetwo[2], S13,  0x242070db);
-    FF(b, c, d, a, M->threetwo[3], S14,  0xc1bdceee);
-    FF(a, b, c, d, M->threetwo[4], S11,  0xf57c0faf);
-    FF(d, a, b, c, M->threetwo[5], S12,  0x4787c62a);
-    FF(c, d, a, b, M->threetwo[6], S13,  0xa8304613);
-    FF(b, c, d, a, M->threetwo[7], S14,  0xfd469501);
-    FF(a, b, c, d, M->threetwo[8], S11,  0x698098d8);
-    FF(d, a, b, c, M->threetwo[9], S12,  0x8b44f7af);
-    FF(c, d, a, b, M->threetwo[10], S13, 0xffff5bb1);
-    FF(b, c, d, a, M->threetwo[11], S14, 0x895cd7be);
-    FF(a, b, c, d, M->threetwo[12], S11, 0x6b901122);
-    FF(d, a, b, c, M->threetwo[13], S12, 0xfd987193);
-    FF(c, d, a, b, M->threetwo[14], S13, 0xa679438e);
-    FF(b, c, d, a, M->threetwo[15], S14, 0x49b40821);
+    FF(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[0], S11, K[0]);  
+    FF(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[1], S12, K[1]); 
+    FF(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[2], S13, K[2]);
+    FF(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[3], S14, K[3]);
+    FF(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[4], S11, K[4]);
+    FF(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[5], S12, K[5]);
+    FF(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[6], S13, K[6]);
+    FF(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[7], S14, K[7]);
+    FF(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[8], S11, K[8]);
+    FF(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[9], S12, K[9]);
+    FF(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[10], S13, K[10]);
+    FF(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[11], S14, K[11]);
+    FF(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[12], S11, K[12]);
+    FF(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[13], S12, K[13]);
+    FF(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[14], S13, K[14]);
+    FF(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[15], S14, K[15]);
 
     // R2
-    GG(a, b, c, d, M->threetwo[1], S21,  0xf61e2562);
-    GG(d, a, b, c, M->threetwo[6], S22,  0xc040b340);
-    GG(c, d, a, b, M->threetwo[11], S23, 0x265e5a51);
-    GG(b, c, d, a, M->threetwo[0], S24,  0xe9b6c7aa);
-    GG(a, b, c, d, M->threetwo[5], S21,  0xd62f105d);
-    GG(d, a, b, c, M->threetwo[10], S22, 0x2441453);
-    GG(c, d, a, b, M->threetwo[15], S23, 0xd8a1e681);
-    GG(b, c, d, a, M->threetwo[4], S24,  0xe7d3fbc8);
-    GG(a, b, c, d, M->threetwo[9], S21,  0x21e1cde6);
-    GG(d, a, b, c, M->threetwo[14], S22, 0xc33707d6);
-    GG(c, d, a, b, M->threetwo[3], S23,  0xf4d50d87);
-    GG(b, c, d, a, M->threetwo[8], S24,  0x455a14ed);
-    GG(a, b, c, d, M->threetwo[13], S21, 0xa9e3e905);
-    GG(d, a, b, c, M->threetwo[2], S22,  0xfcefa3f8);
-    GG(c, d, a, b, M->threetwo[7], S23,  0x676f02d9);
-    GG(b, c, d, a, M->threetwo[12], S24, 0x8d2a4c8a);
+    GG(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[1], S21, K[16]);  
+    GG(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[6], S22, K[17]);  
+    GG(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[11], S23, K[18]); 
+    GG(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[0], S24, K[19]);  
+    GG(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[5], S21, K[20]);  
+    GG(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[10], S22, K[21]);  
+    GG(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[15], S23, K[22]); 
+    GG(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[4], S24, K[23]);  
+    GG(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[9], S21, K[24]);  
+    GG(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[14], S22, K[25]); 
+    GG(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[3], S23, K[26]);  
+    GG(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[8], S24, K[27]);  
+    GG(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[13], S21, K[28]); 
+    GG(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[2], S22, K[29]);  
+    GG(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[7], S23, K[30]);  
+    GG(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[12], S24, K[31]); 
 
     // R3
-    HH(a, b, c, d, M->threetwo[5], S31,  0xfffa3942);
-    HH(d, a, b, c, M->threetwo[8], S32,  0x8771f681);
-    HH(c, d, a, b, M->threetwo[11], S33, 0x6d9d6122);
-    HH(b, c, d, a, M->threetwo[14], S34, 0xfde5380c);
-    HH(a, b, c, d, M->threetwo[1], S31,  0xa4beea44);
-    HH(d, a, b, c, M->threetwo[4], S32,  0x4bdecfa9);
-    HH(c, d, a, b, M->threetwo[7], S33,  0xf6bb4b60);
-    HH(b, c, d, a, M->threetwo[10], S34, 0xbebfbc70);
-    HH(a, b, c, d, M->threetwo[13], S31, 0x289b7ec6);
-    HH(d, a, b, c, M->threetwo[0], S32,  0xeaa127fa);
-    HH(c, d, a, b, M->threetwo[3], S33,  0xd4ef3085);
-    HH(b, c, d, a, M->threetwo[6], S34,  0x4881d05);
-    HH(a, b, c, d, M->threetwo[9], S31,  0xd9d4d039);
-    HH(d, a, b, c, M->threetwo[12], S32, 0xe6db99e5);
-    HH(c, d, a, b, M->threetwo[15], S33, 0x1fa27cf8);
-    HH(b, c, d, a, M->threetwo[2], S34,  0xc4ac5665);
+    HH(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[5], S31, K[32]);  
+    HH(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[8], S32, K[33]);  
+    HH(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[11], S33, K[34]); 
+    HH(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[14], S34, K[35]); 
+    HH(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[1], S31, K[36]);  
+    HH(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[4], S32, K[37]);  
+    HH(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[7], S33, K[38]);  
+    HH(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[10], S34, K[39]); 
+    HH(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[13], S31, K[40]); 
+    HH(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[0], S32, K[41]);  
+    HH(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[3], S33, K[42]);  
+    HH(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[6], S34, K[43]);
+    HH(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[9], S31, K[44]);  
+    HH(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[12], S32, K[45]); 
+    HH(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[15], S33, K[46]); 
+    HH(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[2], S34, K[47]);  
 
     // R4
-    II(a, b, c, d, M->threetwo[0], S41, 0xf4292244);
-    II(d, a, b, c, M->threetwo[7], S42, 0x432aff97);
-    II(c, d, a, b, M->threetwo[14], S43, 0xab9423a7);
-    II(b, c, d, a, M->threetwo[5], S44, 0xfc93a039);
-    II(a, b, c, d, M->threetwo[12], S41, 0x655b59c3);
-    II(d, a, b, c, M->threetwo[3], S42, 0x8f0ccc92);
-    II(c, d, a, b, M->threetwo[10], S43, 0xffeff47d);
-    II(b, c, d, a, M->threetwo[1], S44, 0x85845dd1);
-    II(a, b, c, d, M->threetwo[8], S41, 0x6fa87e4f);
-    II(d, a, b, c, M->threetwo[15], S42, 0xfe2ce6e0);
-    II(c, d, a, b, M->threetwo[6], S43, 0xa3014314);
-    II(b, c, d, a, M->threetwo[13], S44, 0x4e0811a1);
-    II(a, b, c, d, M->threetwo[4], S41, 0xf7537e82);
-    II(d, a, b, c, M->threetwo[11], S42, 0xbd3af235);
-    II(c, d, a, b, M->threetwo[2], S43, 0x2ad7d2bb);
-    II(b, c, d, a, M->threetwo[9], S44, 0xeb86d391);
+    II(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[0], S41, K[48]);  
+    II(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[7], S42, K[49]);  
+    II(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[14], S43, K[50]); 
+    II(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[5], S44, K[51]);  
+    II(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[12], S41, K[52]); 
+    II(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[3], S42, K[53]);  
+    II(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[10], S43, K[54]); 
+    II(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[1], S44, K[55]);  
+    II(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[8], S41, K[56]);  
+    II(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[15], S42, K[57]); 
+    II(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[6], S43, K[58]);  
+    II(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[13], S44, K[59]); 
+    II(Hash[0], Hash[1], Hash[2], Hash[3], M->threetwo[4], S41, K[60]);  
+    II(Hash[3], Hash[0], Hash[1], Hash[2], M->threetwo[11], S42, K[61]); 
+    II(Hash[2], Hash[3], Hash[0], Hash[1], M->threetwo[2], S43, K[62]);  
+    II(Hash[1], Hash[2], Hash[3], Hash[0], M->threetwo[9], S44, K[63]);  
 
-    H[0] += a;
-    H[1] += b;
-    H[2] += c;
-    H[3] += d;
+
+    Hash[0] += H[0];
+    Hash[1] += H[1];
+    Hash[2] += H[2];
+    Hash[3] += H[3];
+
+
 }
 
 
@@ -268,12 +278,13 @@ int main(int argc, char *argv[]) {
         nexthash(&M, H);
     }
 
-    printf("\n Hash value of the file with MD5 algorithm\n");
-
+    
     // print hash
+    printf("File: %s\n Hash Output: ", argv[1]);
+    
     for (int i = 0; i < 4; i++)
 
-        printf("%02" PRIx32 "", H[i]);
+        printf("%02x%02x%02x%02x",(H[i] >> 0 ) & 0xFF, (H[i] >> 8 ) & 0xFF, (H[i] >> 16) & 0xFF, (H[i] >> 24) & 0xFF);
 
     printf("\n");
 
